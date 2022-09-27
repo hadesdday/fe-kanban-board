05b8cdae86d2e80181bf4af19de006da1b1b5c01
Hieu Nguyen <nhocproaoe@gmail.com>
2022-09-27 17:17:48 +0700

    styling card component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app__container">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">App Bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/500/500" alt="404" />
+              This is the title of the card
+            </li>
+            <li>This is the title of the card1</li>
+            <li>This is the title of the card2</li>
+            <li>This is the title of the card3</li>
+            <li>This is the title of the card4</li>
+            <li>This is the title of the card4</li>
+            <li>This is the title of the card4</li>
+            <li>This is the title of the card4</li>
+            <li>This is the title of the card4</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/501/501" alt="404" />
+              This is the title of the card
+            </li>
+            <li>This is the title of the card1</li>
+            <li>This is the title of the card2</li>
+            <li>This is the title of the card3</li>
+            <li>This is the title of the card4</li>
+            <li>This is the title of the card4</li>
+            <li>This is the title of the card4</li>
+            <li>This is the title of the card4</li>
+            <li>This is the title of the card4</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/501/501" alt="404" />
+              This is the title of the card
+            </li>
+            <li>This is the title of the card1</li>
+            <li>This is the title of the card2</li>
+            <li>This is the title of the card3</li>
+            <li>This is the title of the card4</li>
+            <li>This is the title of the card4</li>
+            <li>This is the title of the card4</li>
+            <li>This is the title of the card4</li>
+            <li>This is the title of the card4</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/501/501" alt="404" />
+              This is the title of the card
+            </li>
+            <li>This is the title of the card1</li>
+            <li>This is the title of the card2</li>
+            <li>This is the title of the card3</li>
+            <li>This is the title of the card4</li>
+            <li>This is the title of the card4</li>
+            <li>This is the title of the card4</li>
+            <li>This is the title of the card4</li>
+            <li>This is the title of the card4</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/501/501" alt="404" />
+              This is the title of the card
+            </li>
+            <li>This is the title of the card1</li>
+            <li>This is the title of the card2</li>
+            <li>This is the title of the card3</li>
+            <li>This is the title of the card4</li>
+            <li>This is the title of the card4</li>
+            <li>This is the title of the card4</li>
+            <li>This is the title of the card4</li>
+            <li>This is the title of the card4</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/501/501" alt="404" />
+              This is the title of the card
+            </li>
+            <li>This is the title of the card1</li>
+            <li>This is the title of the card2</li>
+            <li>This is the title of the card3</li>
+            <li>This is the title of the card4</li>
+            <li>This is the title of the card4</li>
+            <li>This is the title of the card4</li>
+            <li>This is the title of the card4</li>
+            <li>This is the title of the card4</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
