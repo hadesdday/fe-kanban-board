bddb46eeec778c9199003ee34f0ccd6b1c5b5cb1
Hieu Nguyen <nhocproaoe@gmail.com>
2022-10-11 22:42:49 +0700

    added some components

--- src/App.js
+++ src/App.js
@@ -1,120 +1,14 @@
 import "./App.scss";
+import AppBar from "./components/AppBar";
+import BoardBar from "./components/BoardBar";
+import BoardContent from "./components/BoardContent";
 
 function App() {
   return (
     <div className="app__container">
-      <nav className="navbar app">App Bar</nav>
-      <nav className="navbar board">App Bar</nav>
-      <div className="board-columns">
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://picsum.photos/500/500" alt="404" />
-              This is the title of the card
-            </li>
-            <li>This is the title of the card1</li>
-            <li>This is the title of the card2</li>
-            <li>This is the title of the card3</li>
-            <li>This is the title of the card4</li>
-            <li>This is the title of the card4</li>
-            <li>This is the title of the card4</li>
-            <li>This is the title of the card4</li>
-            <li>This is the title of the card4</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://picsum.photos/501/501" alt="404" />
-              This is the title of the card
-            </li>
-            <li>This is the title of the card1</li>
-            <li>This is the title of the card2</li>
-            <li>This is the title of the card3</li>
-            <li>This is the title of the card4</li>
-            <li>This is the title of the card4</li>
-            <li>This is the title of the card4</li>
-            <li>This is the title of the card4</li>
-            <li>This is the title of the card4</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://picsum.photos/501/501" alt="404" />
-              This is the title of the card
-            </li>
-            <li>This is the title of the card1</li>
-            <li>This is the title of the card2</li>
-            <li>This is the title of the card3</li>
-            <li>This is the title of the card4</li>
-            <li>This is the title of the card4</li>
-            <li>This is the title of the card4</li>
-            <li>This is the title of the card4</li>
-            <li>This is the title of the card4</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://picsum.photos/501/501" alt="404" />
-              This is the title of the card
-            </li>
-            <li>This is the title of the card1</li>
-            <li>This is the title of the card2</li>
-            <li>This is the title of the card3</li>
-            <li>This is the title of the card4</li>
-            <li>This is the title of the card4</li>
-            <li>This is the title of the card4</li>
-            <li>This is the title of the card4</li>
-            <li>This is the title of the card4</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://picsum.photos/501/501" alt="404" />
-              This is the title of the card
-            </li>
-            <li>This is the title of the card1</li>
-            <li>This is the title of the card2</li>
-            <li>This is the title of the card3</li>
-            <li>This is the title of the card4</li>
-            <li>This is the title of the card4</li>
-            <li>This is the title of the card4</li>
-            <li>This is the title of the card4</li>
-            <li>This is the title of the card4</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://picsum.photos/501/501" alt="404" />
-              This is the title of the card
-            </li>
-            <li>This is the title of the card1</li>
-            <li>This is the title of the card2</li>
-            <li>This is the title of the card3</li>
-            <li>This is the title of the card4</li>
-            <li>This is the title of the card4</li>
-            <li>This is the title of the card4</li>
-            <li>This is the title of the card4</li>
-            <li>This is the title of the card4</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-      </div>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
     </div>
   );
 }
---
+++ src/components/AppBar/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./styles.scss";
+
+function AppBar(props) {
+    return (
+        <>
+            <nav className="navbar-app">App Bar</nav>
+      </>
+    );
+}
+
+export default AppBar;
